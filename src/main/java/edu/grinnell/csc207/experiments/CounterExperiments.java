b25343d71cb73cb20ae4219fa0887c5d3fc03ff7
KHANH <khanhphuongdo28@gmail.com>
2024-10-01 15:17:50 -0500

    Add DoubleCounter class

--- src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
+++ src/main/java/edu/grinnell/csc207/experiments/CounterExperiments.java
@@ -22,7 +22,7 @@ public class CounterExperiments {
 
     // Set up some counters
     NamedCounter alpha = new NamedCounter("alfa", 0);
-    Counter beta = new BasicCounter(123);
+    Counter beta = new DoubleCounter(123);
     DecrementableCounter gamma = new DecrementableCounter(-5);
 
     // Print original values
